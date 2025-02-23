2404e2cd57e6822841b98afdb0cba51702d2fa62
Asephara <e6292743@cmaisonneuve.qc.ca>
2025-02-23 20:33:06 +0000

    ajout de exemple if else

---
+++ exemple1.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+int main(){
+    int nombre =-19;
+    if(nombre <=0){
+        printf("Le nombre est negatif ou nul. \n");
+} else {
+    printf ("le nombre est positif.\n");
+}
+}
